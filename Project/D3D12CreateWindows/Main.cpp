6f8b26b8b88f972983dbb775b3c819008be1ad4e
jjh2v2 <jjh2v2@naver.com>
2017-04-02 02:58:12 +0900

    no message

--- Project/D3D12CreateWindows/Main.cpp
+++ Project/D3D12CreateWindows/Main.cpp
@@ -1,9 +1,115 @@
 #pragma once
 #include "stdafx.h"
 
+class DXSample
+{
+public:
+	std::wstring pname;
+	DXSample() {};
+	DXSample(UINT width, UINT height, std::wstring name){};
+	virtual ~DXSample() {};
+	void OnPrint() {
+		int iii = 0;
+		int www = 0;
+	};
+};
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	DXSample* pSample = reinterpret_cast<DXSample*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		// Save the DXSample* passed in to CreateWindow.
+		LPCREATESTRUCT pCreateStruct = reinterpret_cast<LPCREATESTRUCT>(lParam);
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
+	}
+	return 0;
+
+	case WM_KEYDOWN:
+		if (pSample)
+		{
+			pSample->OnPrint();
+
+			//pSample->OnKeyDown(static_cast<UINT8>(wParam));
+		}
+		return 0;
+
+	case WM_KEYUP:
+		if (pSample)
+		{
+			//pSample->OnKeyUp(static_cast<UINT8>(wParam));
+		}
+		return 0;
+
+	case WM_PAINT:
+		if (pSample)
+		{
+			//pSample->OnUpdate();
+			//pSample->OnRender();
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	// Handle any messages the switch statement didn't.
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
+	// Initialize the window class.
+	WNDCLASSEX windowClass = { 0 };
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.lpszClassName = L"DXSampleClass";
+	RegisterClassEx(&windowClass);
 
-	MessageBox(NULL, TEXT("gggg"), TEXT(""),MB_OK);
+	RECT windowRect = { 0, 0, static_cast<LONG>(800), static_cast<LONG>(800) };
+	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	DXSample* pSample = new DXSample();
+	pSample->pname = L"sdfsd";
+
+	// Create the window and store a handle to it.
+	HWND m_hwnd = CreateWindow(
+		windowClass.lpszClassName,
+		L"",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		nullptr,		// We have no parent window.
+		nullptr,		// We aren't using menus.
+		hInstance,
+		pSample
+		);
+
+	// Initialize the sample. OnInit is defined in each child-implementation of DXSample.
+	//pSample->OnInit();
+
+	ShowWindow(m_hwnd, nCmdShow);
+
+	// Main sample loop.
+	MSG msg = {};
+	while (msg.message != WM_QUIT)
+	{
+		// Process any messages in the queue.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	
 	return 0;
 }
