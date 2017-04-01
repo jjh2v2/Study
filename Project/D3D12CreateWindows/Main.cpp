c481b2c9335fa897d1521c1f9bf4de877d3dfca6
jjh2v2 <jjh2v2@naver.com>
2017-04-02 02:17:56 +0900

    no message

---
+++ Project/D3D12CreateWindows/Main.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+
+	return 1;
+}
---
+++ Project/D3D12CreateWindows/stdafx.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
