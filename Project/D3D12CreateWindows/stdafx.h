263a8a571285eb8ce151f47cbadf5ffd0f07d90d
jjh2v2 <jjh2v2@naver.com>
2017-04-02 10:58:07 +0900

    no message

--- Project/D3D12CreateWindows/stdafx.h
+++ Project/D3D12CreateWindows/stdafx.h
@@ -1,6 +1,18 @@
 
-
 #pragma once
+
+// Link library.
+ #pragma comment(lib, "DXGI.lib")
+ #pragma comment(lib, "D3DCompiler.lib")
+ #pragma comment(lib, "D3D12.lib")
+ #pragma comment(lib, "D2D1.lib")
+ #pragma comment(lib, "DWrite.lib")
+ #pragma comment(lib, "WindowsCodecs.lib")
+ #pragma comment(lib, "Strmiids.lib")
+ #pragma comment(lib, "DSound.lib")
+ //#pragma comment(lib, "DShow.lib")
+ #pragma comment(lib, "DInput8.lib")
+ #pragma comment(lib, "DXGuid.lib")
 
 #include <windows.h>
 
