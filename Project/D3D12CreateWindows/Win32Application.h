fb80f65941b055575592596e0fb07a4beff2abd0
jjh2v2 <jjh2v2@naver.com>
2017-04-02 11:24:23 +0900

    no message

---
+++ Project/D3D12CreateWindows/Win32Application.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreFrameWork.h"
+class CoreFrameWork;
+
+class Win32Application
+{
+public:
+	static int Run(CoreFrameWork* pSample, HINSTANCE hInstance, int nCmdShow);
+	static HWND GetHwnd() { return m_hwnd; }
+
+protected:
+	static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+private:
+	static HWND m_hwnd;
+};
--- Project/D3D12CreateWindows/stdafx.h
+++ Project/D3D12CreateWindows/stdafx.h
@@ -14,6 +14,10 @@
  #pragma comment(lib, "DInput8.lib")
  #pragma comment(lib, "DXGuid.lib")
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
 #include <windows.h>
 
 #include <d3d12.h>
