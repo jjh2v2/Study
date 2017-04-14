18cc3b879d2bf946092f92f6b7dc3cfc1f85ee63
DESKTOP-ND1AQ8M\jjh <jjh2v2@naver.com>
2017-04-14 19:59:36 +0900

    no message

--- Project/D3D12CreateWindows/CoreFrameWork.cpp
+++ Project/D3D12CreateWindows/CoreFrameWork.cpp
@@ -124,10 +124,28 @@ void CoreFrameWork::OnInit()
 
 
 
+	// Create a command allocator.
+	ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+
+	// Create a basic command list.
+	ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), NULL, IID_PPV_ARGS(&m_commandList)));
+
+	// GPU 동기화를위한 울타리를 만듭니다.
+	ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, __uuidof(ID3D12Fence), (void**)&m_fence));
+
+	// 울타리에 대한 이벤트 객체를 만듭니다.
+	m_fenceEvent = CreateEventEx(NULL, FALSE, FALSE, EVENT_ALL_ACCESS);
+
+	// 시작 펜스 값을 초기화합니다.
+	m_fenceValue = 1;
+
+
+
+
+
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	// 스왑 체인을 어떤종류인지 설명하고 생성.
 	////////////////////////////////////////////////////////////////////////////////////////////////
-	static const UINT FrameCount = 2;
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
 	swapChainDesc.BufferCount = FrameCount;
 	swapChainDesc.Width = m_width;
@@ -184,11 +202,191 @@ void CoreFrameWork::OnInit()
 	rtvHeapDesc.NumDescriptors = FrameCount;
 	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
 	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	rtvHeapDesc.NodeMask = 0;
 	ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)));
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	// 현재 장치의 샘플러 설명자 크기를 가져옵니다.
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// 깊이 스텐실 뷰 (DSV) 기술자 힙을 기술하고 생성합니다.
+	// 각 프레임에는 자체 깊이 스텐실이 있습니다 (그림자를 씁니다)
+	// 그러면 장면 자체에 하나가 있습니다.
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc = {};
+	dsvHeapDesc.NumDescriptors = 1;
+	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+	dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	dsvHeapDesc.NodeMask = 0;
+	ThrowIfFailed(m_device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&m_dsvHeap)));
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// Create frame resources.
+	// Create a RTV for each frame.
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+	for (UINT n = 0; n < FrameCount; n++)
+	{
+		ThrowIfFailed(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
+		m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
+		rtvHandle.Offset(1, m_rtvDescriptorSize);
+	}
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// Create frame resources.
+	// Create the depth/stencil buffer and view.
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	D3D12_RESOURCE_DESC depthStencilDesc;
+	depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	depthStencilDesc.Alignment = 0;
+	depthStencilDesc.Width = m_width;
+	depthStencilDesc.Height = m_height;
+	depthStencilDesc.DepthOrArraySize = 1;
+	depthStencilDesc.MipLevels = 1;
+
+	// Correction 11/12/2016: SSAO chapter requires an SRV to the depth buffer to read from 
+	// the depth buffer.  Therefore, because we need to create two views to the same resource:
+	//   1. SRV format: DXGI_FORMAT_R24_UNORM_X8_TYPELESS
+	//   2. DSV Format: DXGI_FORMAT_D24_UNORM_S8_UINT
+	// we need to create the depth buffer resource with a typeless format.  
+	depthStencilDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
+
+	depthStencilDesc.SampleDesc.Count = 1;
+	depthStencilDesc.SampleDesc.Quality = 0;
+	depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+	D3D12_CLEAR_VALUE optClear;
+	optClear.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	optClear.DepthStencil.Depth = 1.0f;
+	optClear.DepthStencil.Stencil = 0;
+	ThrowIfFailed(m_device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&depthStencilDesc,
+		D3D12_RESOURCE_STATE_COMMON,
+		&optClear,
+		IID_PPV_ARGS(mDepthStencilBuffer.GetAddressOf())));
+
+	// Create descriptor to mip level 0 of entire resource using the format of the resource.
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc;
+	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	dsvDesc.Texture2D.MipSlice = 0;
+	m_device->CreateDepthStencilView(mDepthStencilBuffer.Get(), &dsvDesc, m_dsvHeap->GetCPUDescriptorHandleForHeapStart());
+
+	// Transition the resource from its initial state to be used as a depth buffer.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mDepthStencilBuffer.Get(),
+		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_DEPTH_WRITE));
+
+
+
+	
+	// Update the viewport transform to cover the client area.
+	mScreenViewport.TopLeftX = 0;
+	mScreenViewport.TopLeftY = 0;
+	mScreenViewport.Width = static_cast<float>(m_width);
+	mScreenViewport.Height = static_cast<float>(m_height);
+	mScreenViewport.MinDepth = 0.0f;
+	mScreenViewport.MaxDepth = 1.0f;
+	mScissorRect = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
+
+
+
+
+	// 처음에는 레코딩 상태에서 생성 될 때 초기화하는 동안 명령 목록을 닫아야합니다.
+	ThrowIfFailed(m_commandList->Close());
+
 	int iiiiiii = 0;
+}
+
+void CoreFrameWork::OnRender()
+{
+	D3D12_RESOURCE_BARRIER barrier;
+	D3D12_CPU_DESCRIPTOR_HANDLE renderTargetViewHandle;
+	unsigned int renderTargetViewDescriptorSize;
+	float color[4];
+	ID3D12CommandList* ppCommandLists[1];
+	UINT64 fenceToWaitFor;
+
+
+	// Reset (re-use) the memory associated command allocator.
+	ThrowIfFailed(m_commandAllocator->Reset());
+
+	// Reset the command list, use empty pipeline state for now since there are no shaders and we are just clearing the screen.
+	ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()));
+
+	m_commandList->RSSetViewports(1, &mScreenViewport);
+	m_commandList->RSSetScissorRects(1, &mScissorRect);
+
+	// Record commands in the command list now.
+	// Start by setting the resource barrier.
+	barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+	barrier.Transition.pResource = m_renderTargets[m_frameIndex].Get();
+	barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+	barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+	barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+	m_commandList->ResourceBarrier(1, &barrier);
+
+	// Get the render target view handle for the current back buffer.
+	renderTargetViewHandle = m_rtvHeap->GetCPUDescriptorHandleForHeapStart();
+	renderTargetViewDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	if (m_frameIndex == 1)
+	{
+		renderTargetViewHandle.ptr += renderTargetViewDescriptorSize;
+	}
+
+	// Set the back buffer as the render target.
+	m_commandList->OMSetRenderTargets(1, &renderTargetViewHandle, FALSE, NULL);
+
+	// Then set the color to clear the window to.
+	color[0] = 0.1f;
+	color[1] = 0.1f;
+	color[2] = 0.5f;
+	color[3] = 1.0f;
+	m_commandList->ClearRenderTargetView(renderTargetViewHandle, color, 0, NULL);
+
+	// Indicate that the back buffer will now be used to present.
+	barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+	m_commandList->ResourceBarrier(1, &barrier);
+
+	// Close the list of commands.
+	ThrowIfFailed(m_commandList->Close());
+
+	// Load the command list array (only one command list for now).
+	ppCommandLists[0] = m_commandList.Get();
+
+	// Execute the list of commands.
+	m_commandQueue->ExecuteCommandLists(1, ppCommandLists);
+
+	ThrowIfFailed(m_swapChain->Present(0, 0));
+
+	// Signal and increment the fence value.
+	fenceToWaitFor = m_fenceValue;
+	ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fenceToWaitFor));
+	m_fenceValue++;
+
+	// Wait until the GPU is done rendering.
+	if (m_fence->GetCompletedValue() < fenceToWaitFor)
+	{
+		ThrowIfFailed(m_fence->SetEventOnCompletion(fenceToWaitFor, m_fenceEvent));
+		WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+
+	// Alternate the back buffer index back and forth between 0 and 1 each frame.
+	m_frameIndex == 0 ? m_frameIndex = 1 : m_frameIndex = 0;
+
 }
--- Project/D3D12CreateWindows/CoreFrameWork.h
+++ Project/D3D12CreateWindows/CoreFrameWork.h
@@ -15,7 +15,7 @@ public:
 	//virtual void OnDestroy() = 0;
 	virtual void OnInit();
 	virtual void OnUpdate() {};
-	virtual void OnRender() {};
+	virtual void OnRender();
 	virtual void OnDestroy() {};
 
 	// Samples override the event handlers to handle specific messages.
@@ -45,12 +45,28 @@ private:
 	std::wstring m_title;
 
 private:
+	ComPtr<ID3D12PipelineState> m_pipelineState;
+	static const UINT FrameCount = 2;
+	ComPtr<ID3D12Resource> mDepthStencilBuffer;
+	ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+	ComPtr<ID3D12DescriptorHeap> m_dsvHeap;
 	UINT m_rtvDescriptorSize;
 	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
 	UINT m_frameIndex;
 	ComPtr<IDXGISwapChain3> m_swapChain;
 	ComPtr<ID3D12Device> m_device;
 	ComPtr<ID3D12CommandQueue> m_commandQueue;
+
+	
+	ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+	ComPtr<ID3D12GraphicsCommandList> m_commandList;
+	ComPtr<ID3D12Fence> m_fence;
+	UINT64 m_fenceValue;
+	HANDLE m_fenceEvent;
+
+	D3D12_VIEWPORT mScreenViewport;
+	D3D12_RECT mScissorRect;
+
 
 protected:
 	void GetHardwareAdapter(_In_ IDXGIFactory2* pFactory, _Outptr_result_maybenull_ IDXGIAdapter1** ppAdapter);
